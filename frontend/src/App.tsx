1e7941b1e06057c09ece426c7ccb3bf10d6ac7e2
prrtek <prrtek@outlook.com>
2024-03-15 13:09:01 +0530

    Add new pages and components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,35 +1,78 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {
+  Navigate,
+  Route,
+  BrowserRouter as Router,
+  Routes,
+} from "react-router-dom";
+import Layout from "./layouts/Layout"; // Import the Layout component
+import Home from "./pages/Home";
+import Detail from "./pages/Detail";
+import Search from "./pages/Search";
+import Register from "./pages/Register";
+import SignIn from "./pages/SignIn";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
+  // const { isLoggedIn } = useAppContext();
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <Router>
+      <Routes>
+        <Route
+          path='/'
+          element={
+            <Layout>
+              <Home />
+            </Layout>
+          }
+        />
+        <Route
+          path='/search'
+          element={
+            <Layout>
+              <Search />
+            </Layout>
+          }
+        />
+        <Route
+          path='/detail/:hotelId'
+          element={
+            <Layout>
+              <Detail />
+            </Layout>
+          }
+        />
+        <Route
+          path='/register'
+          element={
+            <Layout>
+              <Register />
+            </Layout>
+          }
+        />
+        <Route
+          path='/sign-in'
+          element={
+            <Layout>
+              <SignIn />
+            </Layout>
+          }
+        />
 
-export default App
+        {/* 
+        {isLoggedIn && (
+          <>
+            <Route path='/hotel/:hotelId/booking' element={<Layout><Booking /></Layout>} />
+            <Route path='/add-hotel' element={<Layout><AddHotel /></Layout>} />
+            <Route path='/edit-hotel/:hotelId' element={<Layout><EditHotel /></Layout>} />
+            <Route path='/my-hotels' element={<Layout><MyHotels /></Layout>} />
+            <Route path='/my-bookings' element={<Layout><MyBookings /></Layout>} />
+          </>
+        )} 
+        */}
+
+        <Route path='*' element={<Navigate to='/' />} />
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
---
+++ frontend/src/components/Header.tsx
@@ -0,0 +1,20 @@
+import { Link } from "react-router-dom";
+
+const Header = () => {
+  return (
+    <div className='bg-blue-800 py-6'>
+      <div className='container mx-auto flex justify-between'>
+        <span className='text-3xl text-white font-bold tracking-tight'>
+          StayQuest
+        </span>
+        <span className='flex space-x-2'>
+          <span className='flex items-center text-blue-600 px-3 font-bold hover:bg-gray-700'>
+            Sign In
+          </span>
+        </span>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
